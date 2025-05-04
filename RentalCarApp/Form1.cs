7cef23019b735b7f4b1609efa60ad366eeb3103d
Lungsta <seekhid@gmail.com>
2025-05-04 22:55:29 +0200

    Completed windows app design wiyh verification

--- RentalCarApp/Form1.cs
+++ RentalCarApp/Form1.cs
@@ -25,25 +25,44 @@ namespace RentalCarApp
                 String CustomerName = txtCName.Text;
                 var DateRented = dtpDateRented.Value;
                 var DateReturned = dtpDateReturned.Value;
-                var TypeOfCar = cmbTypeOfCar.SelectedItem.ToString();
+                var TypeOfCar = cmbTypeOfCar.Text;
                 double Cost = Convert.ToDouble(txtCost.Text);
                 var isValid = true;
+                var errorMessage = "";
 
                 if (string.IsNullOrWhiteSpace(CustomerName) || string.IsNullOrWhiteSpace(TypeOfCar))
                 {
                     isValid = false;
-                    MessageBox.Show("Please fill in all fields");
+                    errorMessage += "Error: Please enter missing data.\n\r";
+                }
+                
+
+                if (string.IsNullOrWhiteSpace(CustomerName) || string.IsNullOrWhiteSpace(TypeOfCar))
+                {
+                    isValid = false;
+                    errorMessage += "Error: Please enter missing data.\n\r";
+                    //MessageBox.Show("Please fill in all fields");
                 }
 
                 if (DateRented > DateReturned)
                 {
                     isValid = false;
-                    MessageBox.Show("Invalid date selection");
+                    errorMessage += "Error: Illegal date selection.\n\r";
                 }
 
-                if (isValid == true)
+                //if (isValid == true)
+                if(isValid)
                 {
-                    MessageBox.Show($"Thank you {CustomerName} for renting with us!\n\r" + $"Pick up date: {DateRented}\n\r" + $"Vehicle return date: {DateReturned}\n\r" + $"Vehicle rented: {TypeOfCar}\n" + $"Cost: {Cost}\n" + $"Thank you for your support!!");
+                    MessageBox.Show($"Thank you {CustomerName} for renting with us!\n\r" + 
+                        $"Pick up date: {DateRented}\n\r" + 
+                        $"Vehicle return date: {DateReturned}\n\r" + 
+                        $"Vehicle rented: {TypeOfCar}\n" + 
+                        $"Cost: {Cost}\n" + 
+                        $"Thank you for your support!!");
+                }
+                else
+                {
+                    MessageBox.Show(errorMessage);
                 }
             }
             catch (Exception ex)
